d66eab9570da7d4015c333aec43aba1434ee881e
马文卿 <529837505@qq.com>
2022-12-06 15:27:40 +0800

    finish proxy

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,22 +1,35 @@
 import { isObj } from "@vue/shared";
 
+const reactiveMap = new WeakMap();
+const enum ReactiveFlags {
+    IS_REACTIVE = '_v_isReactive'
+}
+
 export function reactive(target) {
     if(!isObj(target)) {
         return target
+    }
+    if(target[ReactiveFlags.IS_REACTIVE]) {
+        return target
+    }
+    const existing = reactiveMap.get(target);
+    if(existing) {
+        return existing
     }
 
     const proxy = new Proxy(target, {
         get(target, key, receiver) {
             console.log('get');
-            
-            return target[key]
+            if(key === ReactiveFlags.IS_REACTIVE) {
+                return true
+            }
+            return Reflect.get(target, key, receiver)
         },
         set(target, key, value, receiver) {
             console.log('set');
-            
-            target[key] = value;
-            return true
+            return Reflect.set(target, key, value, receiver)
         }
     });
+    reactiveMap.set(target, proxy)
     return proxy;
 }
